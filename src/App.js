d09c6b533078cbde1d513adf328cf6a3d8a1623f
taoyingsong <yingsongtys@163.com>
2016-04-14 19:50:47 +0800

    react-hot 构建多入口

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
 
         return (
     		<div>
-		    	<h1 className="h1">Hello, world. ffd<span>xinzeng span hao </span></h1>
+		    	<h1 className="h1">Hello, world. ffd<span>xinzeng span</span></h1>
 		    	<img src="src/imgs/home.jpg" width="900" height="700" title="home" alt="home"/> <br />
 		    	<img src="src/imgs/whathas.png" title="whathas" alt="whathas"/>
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,19 @@ var webpack = require('webpack');
 var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
+  entry: { 'index': [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './src/index'
+    ], 'test': [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './src/test'
+    ]
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
     publicPath: '/static/'
   },
   plugins: [
@@ -26,6 +31,7 @@ module.exports = {
       // include 和 exclude 不知道是不是只用设置一个就好了
       { test: /\.js[x]?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'], include: path.join(__dirname, 'src') }, 
       { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
     ]
   }
